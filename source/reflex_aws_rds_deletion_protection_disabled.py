624a643ad0f8dd2b503a59ff5e5c39aad172185b
rjulian <richard@rjulian.net>
2020-09-13 14:47:53 -0700

    Semi-automated multi account push

--- source/reflex_aws_rds_deletion_protection_disabled.py
+++ source/reflex_aws_rds_deletion_protection_disabled.py
@@ -4,7 +4,7 @@ import json
 import os
 
 import boto3
-from reflex_core import AWSRule
+from reflex_core import AWSRule, subscription_confirmation
 
 
 class RDSDeletionProtectionDisabled(AWSRule):
@@ -33,5 +33,9 @@ class RDSDeletionProtectionDisabled(AWSRule):
 
 def lambda_handler(event, _):
     """ Handles the incoming event """
+    print(event)
+    if subscription_confirmation.is_subscription_confirmation(event):
+        subscription_confirmation.confirm_subscription(event)
+        return
     rule = RDSDeletionProtectionDisabled(json.loads(event["Records"][0]["body"]))
     rule.run_compliance_rule()
